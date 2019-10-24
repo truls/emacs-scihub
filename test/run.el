b250a83f9d9ee60b418e9c18a3357e00b0e27ffb
Truls Asheim <truls@asheim.dk>
2019-10-24 03:51:13 +0200

    Increase test startup wait

--- test/run.el
+++ test/run.el
@@ -8,7 +8,7 @@
 ;; This is a massive hack, but using async-let here seems the emacs
 ;; init process to finish before we start the tests. If we execute the
 ;; tests synchronously during init emacs will freeze
-(async-let ((x (sleep-for 0.5)))
+(async-let ((x (sleep-for 2)))
   (let ((buf  (get-buffer-create "*Buttercup*")))
     (display-buffer buf)
     (delete-other-windows (get-buffer-window buf t))
